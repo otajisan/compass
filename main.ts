3281d182eb20248a1b5ddac018b4dc487293b66b
Taji <taji.taji.otajisan@gmail.com>
2021-04-04 17:13:44 +0900

    add: scratch image

--- main.ts
+++ main.ts
@@ -1,21 +1,18 @@
 let direction = 0
 let delta = 0
 led.setBrightness(30)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     delta = 5
     direction = input.compassHeading()
     if (315 <= direction && direction < 360 || 0 <= direction && direction < 45) {
         if (direction == 0) {
             music.playTone(494, music.beat(BeatFraction.Eighth))
         }
-        
         if (365 - delta <= direction || direction <= 0 + delta) {
             led.setBrightness(255)
         } else {
             led.setBrightness(30)
         }
-        
         basic.showLeds(`
             # . . . #
             # # . . #
@@ -27,13 +24,11 @@ basic.forever(function on_forever() {
         if (direction == 90) {
             music.playTone(494, music.beat(BeatFraction.Eighth))
         }
-        
         if (90 - delta <= direction && direction <= 90 + delta) {
             led.setBrightness(255)
         } else {
             led.setBrightness(30)
         }
-        
         basic.showLeds(`
             # # # # #
             # . . . .
@@ -45,13 +40,11 @@ basic.forever(function on_forever() {
         if (direction == 180) {
             music.playTone(494, music.beat(BeatFraction.Eighth))
         }
-        
         if (180 - delta <= direction && direction <= 180 + delta) {
             led.setBrightness(255)
         } else {
             led.setBrightness(30)
         }
-        
         basic.showLeds(`
             # # # # #
             # . . . .
@@ -63,13 +56,11 @@ basic.forever(function on_forever() {
         if (direction == 270) {
             music.playTone(494, music.beat(BeatFraction.Eighth))
         }
-        
         if (270 - delta <= direction && direction <= 270 + delta) {
             led.setBrightness(255)
         } else {
             led.setBrightness(30)
         }
-        
         basic.showLeds(`
             # . # . #
             # . # . #
@@ -80,5 +71,4 @@ basic.forever(function on_forever() {
     } else {
         basic.clearScreen()
     }
-    
 })
